f19ed963784ac781bfbee3aa20f8214bcb525706
TarunSanders <sandtm1703@gmail.com.au>
2019-02-21 20:16:17 +0800

    rendered options form with select

--- mt_django_project/view.py
+++ mt_django_project/view.py
@@ -15,26 +15,31 @@ def index(request):
 
 	FuelData = sortedFuel(suburbAndsurrounding,product_num)
 	
-	fuel_data_rows_string = """
-        <form autocomplete="on">
-			<select name = "product" autocomplete="on">
-				<option value = "1" {unl} > Unleaded </option>
-				<option value = "2" {prem} > Premium Unleaded </option>
-				<option value = "3"{dies} > Diesel </option>
-				<option value = "4" {l} > LPG </option>
-				<option value = "5" {bd}> Branded Diesel </option>
-			
-			</select>
+	
+	def ProdForm(i):
+		
+		prodName = ['Unleaded','Premium Unleaded','Diesel', 'LPG', 'Branded Diesel']
+		
+		
+		prodString = ''.join("""
+								<option value = {id}{select}> {prod} </option>
+							""".format(id = str(j), prod = entry, select = 'selected' if j == i else '')
+							for j, entry in enumerate(prodName)
+							)
+		prodString = '<select name = "product" autocomplete="on">' + prodString + '</select>'
+		return prodString
+	
 
-            Suburb and surrounding (type 'metro' for all metro regions): <input type="text" name="suburb" value ="{s}">
-  			<button type="submit"> Enter </button> 
-		</form>
-    """.format(unl = 'selected' if product_num == '1' else "",prem = 'selected' if product_num == '2' else "",dies = 'selected' if product_num =='3' else "",l = 'selected' if product_num =='4' else "",bd= 'selected' if product_num =='5' else "",s=suburbAndsurrounding)
-
-	fuel_data_rows_string = "<html><body>" + fuel_data_rows_string + createfuelHTMLTABLE(FuelData)+"</body></html>"
+	suburbForm = """
+				Suburb and surrounding (type 'metro' for all metro regions): <input type="text" name="suburb" value ="{s}">
+  			    <button type="submit"> Enter </button> 
+				 """.format(s=suburbAndsurrounding)
+	
+	
+	prod = int(product_num) if product_num != None else 0
+	
+	
+	fuel_data_rows_string = "<html><body>" + "<form autocomplete='on'>" + ProdForm(prod) + suburbForm + "</form>"+createfuelHTMLTABLE(FuelData)+"</body></html>"
     
 	return HttpResponse(fuel_data_rows_string)
 
-#def index(request):
-	
-#	return render(request,'fuel.html') 
