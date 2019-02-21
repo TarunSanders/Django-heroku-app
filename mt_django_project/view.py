b08c6fa577078251b82aec6794010bc375cb180a
TarunSanders <sandtm1703@gmail.com.au>
2019-02-21 23:09:27 +0800

    fixing indentations test 1

--- mt_django_project/view.py
+++ mt_django_project/view.py
@@ -1,45 +1,42 @@
 from django.http import HttpResponse
 from mt_django_project.fuelwatch2 import sortedFuel, createfuelHTMLTABLE
 from django.shortcuts import render
-#def index(request):
-#	num = 1
-#	return HttpResponse('<p> my favourite number is {} </p?'.format(num))
-
-#from django.shortcuts import render
+    
 def index(request):
 
-	product_num = request.GET.get('product')
+    product_num = request.GET.get('product')
 	
-	suburbAndsurrounding = request.GET.get('suburb')
+    suburbAndsurrounding = request.GET.get('suburb')
+    
+    FuelData = sortedFuel(suburbAndsurrounding,product_num)
 	
-
-	FuelData = sortedFuel(suburbAndsurrounding,product_num)
 	
-	
-	def ProdForm(i):
+    def ProdForm(i):
 		
-		prodName = ['Unleaded','Premium Unleaded','Diesel', 'LPG', 'Branded Diesel']
-		
-		
-		prodString = ''.join("""
-								<option value = {id} {select}> {prod} </option>
-							""".format(id = str(j+1), prod = entry, select = 'selected' if j+1 == i else '')
+        prodName = ['Unleaded','Premium Unleaded','Diesel', 'LPG', 'Branded Diesel']
+        
+        prodString = ''.join("""
+                                <option value = {id} {select}> {prod} </option>
+							 """.format(id = str(j+1), prod = entry, select = 'selected' if j+1 == i else '')
 							for j, entry in enumerate(prodName)
 							)
-		prodString = '<select name = "product" autocomplete="on">' + prodString + '</select>'
-		return prodString
+       
+
+        prodString = '<select name = "product" autocomplete="on">' + prodString + '</select>'
+		
+        return prodString
 	
 
-	suburbForm = """
-				Suburb and surrounding (type 'metro' for all metro regions): <input type="text" name="suburb" value ="{s}">
-  			    <button type="submit"> Enter </button> 
+    suburbForm = """
+                    Suburb and surrounding (type 'metro' for all metro regions): <input type="text" name="suburb" value ="{s}">
+                    <button type="submit"> Enter </button> 
 				 """.format(s=suburbAndsurrounding)
 	
 	
-	num = int(product_num) if product_num != None else 0
+    
+    num = int(product_num) if product_num != None else 0
 	
-	
-	fuel_data_rows_string = "<html><body>" + "<form autocomplete='on'>" + ProdForm(num) + suburbForm + "</form>"+createfuelHTMLTABLE(FuelData)+"</body></html>"
-    #print(prodString)
-	return HttpResponse(fuel_data_rows_string)
+    fuel_data_rows_string = "<html><body>" + "<form autocomplete='on'>" + ProdForm(num) + suburbForm + "</form>"+createfuelHTMLTABLE(FuelData)+"</body></html>"
+   
+    return HttpResponse(fuel_data_rows_string)
 
